a62779e457f3c94020eb2bbed5601de07f80a6c7
Thorugoh <thorugoh@live.com>
2014-10-14 23:03:34 -0300

    Adicionando Agenda

---
+++ Kosware_HelperDealer/Victor/ModeloDominio/Agenda.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Victor.ModeloDominio
+{
+    class Agenda
+    {
+        DateTime DataAtual = DateTime.Now;
+        DateTime UltimaVisita;
+        DateTime ProximaVisita;
+      
+        public void CadastraVisita()
+        {
+            UltimaVisita = DateTime.Now;
+            AgendarProximaVisita();
+        }
+        public void AgendarProximaVisita()
+        {
+            ProximaVisita = UltimaVisita.AddDays(30);
+        }
+        public string Notificador()
+        {
+           string notificacao = " "; 
+           if  (DataAtual == ProximaVisita)
+           {
+                notificacao = "Próxima Visita: Hoje";
+           }
+           else if (DataAtual != ProximaVisita)
+           {
+               notificacao = "Não há visitas marcadas para hoje";
+           }
+           return notificacao;
+        }  
+    }
+}
+
+    
+
---
+++ Kosware_HelperDealer/Victor/Telas/Principal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Victor.ModeloDominio;
+
+namespace Victor.Telas
+{
+    class Principal
+    {
+        public static void Main()
+        {
+            Agenda agenda = new Agenda();
+            string notifica = agenda.Notificador();
+
+            Console.WriteLine(" -- {0}", notifica);
+
+        }
+ 
+    }
+}
